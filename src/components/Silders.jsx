a490df164532a0d27d3e98843008a2e381a478cc
riazuddin4 <mdriaj2010bd@gmail.com>
2026-05-03 20:58:02 +0600

    silder edit is finished.

--- src/components/Silders.jsx
+++ src/components/Silders.jsx
@@ -1,64 +1,3 @@
-// "use client"
-// import React from 'react';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-
-// import 'swiper/css';
-// import 'swiper/css/pagination';
-
-// import { Pagination } from 'swiper/modules';
-
-// export default function Silders() {
-//     return (
-//         <div className="h-screen w-full">
-//             <Swiper
-//                 className="h-full"
-//                 spaceBetween={50}
-//                 pagination={{ clickable: true }}
-//                 modules={[Pagination]}
-//                 nested={true}
-//             >
-//                 <SwiperSlide className="flex items-center justify-center bg-blue-200">
-//                     Horizontal Slide 1
-//                 </SwiperSlide>
-
-//                 <SwiperSlide className="h-full">
-//                     <Swiper
-//                         direction="vertical"
-//                         className="h-full"
-//                         spaceBetween={50}
-//                         pagination={{ clickable: true }}
-//                         modules={[Pagination]}
-//                         nested={true}
-//                     >
-//                         <SwiperSlide className="flex items-center justify-center bg-red-200">
-//                             Vertical Slide 1
-//                         </SwiperSlide>
-//                         <SwiperSlide className="flex items-center justify-center bg-red-300">
-//                             Vertical Slide 2
-//                         </SwiperSlide>
-//                         <SwiperSlide className="flex items-center justify-center bg-red-400">
-//                             Vertical Slide 3
-//                         </SwiperSlide>
-//                         <SwiperSlide className="flex items-center justify-center bg-red-500">
-//                             Vertical Slide 4
-//                         </SwiperSlide>
-//                         <SwiperSlide className="flex items-center justify-center bg-red-600">
-//                             Vertical Slide 5
-//                         </SwiperSlide>
-//                     </Swiper>
-//                 </SwiperSlide>
-
-//                 <SwiperSlide className="flex items-center justify-center bg-green-200">
-//                     Horizontal Slide 3
-//                 </SwiperSlide>
-
-//                 <SwiperSlide className="flex items-center justify-center bg-yellow-200">
-//                     Horizontal Slide 4
-//                 </SwiperSlide>
-//             </Swiper>
-//         </div>
-//     );
-// }
 "use client"
 
 import React, { useEffect, useState } from 'react';
@@ -73,7 +12,7 @@ export default function Sliders() {
     const [slides, setSlides] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:3000/data.json')
+        fetch('https://books-store-fawn.vercel.app/data.json')
             .then(res => res.json())
             .then(data => setSlides(data));
     }, []);
